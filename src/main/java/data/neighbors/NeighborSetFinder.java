bf9a70344bf89a2d0cb06b81fc503e52954216f0
kvef <162893530+kvef@users.noreply.github.com>
2021-07-31 21:28:49 +0000

    Update Algorithms

--- src/main/java/data/neighbors/NeighborSetFinder.java
+++ src/main/java/data/neighbors/NeighborSetFinder.java
@@ -65,4 +65,100 @@ public class NeighborSetFinder implements Serializable {
     // CombinedMetric object for distance calculations.
     private CombinedMetric cmet = null;
     // The k-nearest neighbor sets. Each row in the table contains the indexes
-    // of the k-nearest neighbors for the g
+    // of the k-nearest neighbors for the given point in the provided DataSet.
+    private int[][] kNeighbors = null;
+    // Distances to the k-nearest neighbors for each point.
+    private float[][] kDistances = null;
+    // The current length of the kNN sets, used during kNN calculations.
+    private int[] kCurrLen = null;
+    // The neighbor occurrence frequencies.
+    private int[] kNeighborFrequencies = null;
+    // The bad neighbor occurrence frequencies.
+    private int[] kBadFrequencies = null;
+    // The good neighbor occurrence frequencies.
+    private int[] kGoodFrequencies = null;
+    // Reverse neighbor sets.
+    private ArrayList<Integer>[] reverseNeighbors = null;
+    // Boolean flag indicating whether the distance matrix was provided.
+    private boolean distancesCalculated = false;
+    // Variance of the distance values.
+    private double distVariance = 0;
+    // Mean of the distance value.
+    private double distMean = 0;
+    // Mean of the neighbor occurrence frequency.
+    private double meanOccFreq;
+    // Standard deviation of the neighbor occurrence frequency.
+    private double stDevOccFreq;
+    // Mean of the detrimental occurrence frequency.
+    private double meanOccBadness = 0;
+    // Standard deviation of the detrimental occurrence frequency.
+    private double stDevOccBadness = 0;
+    // Mean of the beneficial neighbor occurrence frequency.
+    private double meanOccGoodness = 0;
+    // Standard deviation of the beneficial neighbor occurrence frequency.
+    private double stDevOccGoodness = 0;
+    // Mean of the difference between the good and the bad occurrence counts.
+    private double meanGoodMinusBadness = 0;
+    // Mean of the normalized difference between the good and bad occurrence
+    // counts.
+    private double meanRelativeGoodMinusBadness = 0;
+    // Standard deviation of the difference between the good and the bad
+    // occurrence counts.
+    private double stDevGoodMinusBadness = 0;
+    // Standard deviation of the normalized difference between the good and bad
+    // occurrence counts.
+    private double stDevRelativeGoodMinusBadness = 0;
+    // Entropies of the direct kNN sets.
+    private float[] kEntropies = null;
+    // Entropies of the reverse kNN sets.
+    private float[] kRNNEntropies = null;
+    // The currently operating neighborhood size.
+    private int currK;
+    // Small datasets can be extended by synthetic instances from the Gaussian
+    // data model.
+
+    /**
+     * The default constructor.
+     */
+    public NeighborSetFinder() {
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param dset DataSet object that holds the data to calculate the kNN sets
+     * for.
+     * @param cmet CombinedMetric object for distance calculations.
+     */
+    public NeighborSetFinder(DataSet dset, CombinedMetric cmet) {
+        this.dset = dset;
+        this.cmet = cmet;
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param dset DataSet object that holds the data to calculate the kNN sets
+     * for.
+     * @param distMatrix float[][] that is the upper triangular distance matrix.
+     * @param cmet CombinedMetric object for distance calculations.
+     */
+    public NeighborSetFinder(DataSet dset, float[][] distMatrix,
+            CombinedMetric cmet) {
+        this.dset = dset;
+        this.distMatrix = distMatrix;
+        this.cmet = cmet;
+        if (distMatrix == null) {
+            try {
+                distMatrix = dset.calculateDistMatrix(cmet);
+            } catch (Exception e) {
+            }
+        }
+        distancesCalculated = true;
+        try {
+            calculateOccFreqMeanAndVariance();
+        } catch (Exception e) {
+            System.err.println("NSF constructor error.");
+            System.err.println(e.getMessage());
+        }
+  
