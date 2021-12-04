34553199a7fe9f95a667feb962276a9cdbfe60d5
kvef <162893530+kvef@users.noreply.github.com>
2021-12-04 06:42:17 +0000

    refactor: refine code

--- src/main/java/dimensionality_reduction/RandomProjection.java
+++ src/main/java/dimensionality_reduction/RandomProjection.java
@@ -20,4 +20,121 @@ import data.representation.DataInstance;
 import data.representation.DataSet;
 import data.representation.util.DataMineConstants;
 import ioformat.IOARFF;
-import ioformat.SupervisedLoad
+import ioformat.SupervisedLoader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Random;
+import transformation.TransformationInterface;
+import util.CommandLineParser;
+
+/**
+ * This class implements the random projections method where the data is
+ * projected onto a lower-dimensional subspace through the origin via a random
+ * matrix whose columns sum up to 1.
+ *
+ * @author Nenad Tomasev <nenad.tomasev at gmail.com>
+ */
+public class RandomProjection implements TransformationInterface {
+
+    private DataSet dset;
+    // The number of dimensions to reduce the data to.
+    private int targetDimensionality = 2;
+
+    /**
+     * The default constructor.
+     */
+    public RandomProjection() {
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param dset DataSet to reduce the dimensionality of.
+     */
+    public RandomProjection(DataSet dset) {
+        this.dset = dset;
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param dset DataSet to reduce the dimensionality of.
+     * @param targetDimensionality Integer that is the number of dimensions to
+     * reduce the data to.
+     */
+    public RandomProjection(DataSet dset, int targetDimensionality) {
+        this.dset = dset;
+        this.targetDimensionality = targetDimensionality;
+    }
+
+    /**
+     * Set the original data to reduce dimensionality of.
+     *
+     * @param dset DataSet - the original data set.
+     */
+    @Override
+    public void setData(DataSet dset) {
+        this.dset = dset;
+    }
+
+    /**
+     * Generates a random projection matrix.
+     *
+     * @param dataDimensionality Integer that is the number of dimensions in the
+     * data.
+     * @param targetDimensionality Integer that is the number of dimensions to
+     * reduce the data to.
+     * @return float[][] that is the random projection matrix.
+     */
+    public float[][] generateRandomProjectionMatrix(int dataDimensionality,
+            int targetDimensionality) {
+        float[][] projectionMatrix =
+                new float[targetDimensionality][dataDimensionality];
+        float[] column;
+        float denominator;
+        Random randa = new Random();
+        // Just in case, though this should never happen (if all zeroes are
+        // generated randomly for a column).
+        boolean invalid;
+        // Generate random columns.
+        for (int i = 0; i < dataDimensionality; i++) {
+            column = new float[targetDimensionality];
+            invalid = true;
+            do {
+                denominator = 0;
+                for (int j = 0; j < targetDimensionality; j++) {
+                    column[j] = randa.nextFloat();
+                    denominator = Math.max(denominator, column[j]);
+                }
+                if (denominator > 0) {
+                    invalid = false;
+                }
+            } while (invalid);
+            for (int j = 0; j < targetDimensionality; j++) {
+                projectionMatrix[j][i] = column[j] / denominator;
+            }
+        }
+        return projectionMatrix;
+    }
+
+    @Override
+    public DataSet transformData() {
+        // First handle the trivial cases.
+        if (dset == null || dset.isEmpty()) {
+            return null;
+        }
+        int numFAtt = dset.getNumFloatAttr();
+        if (numFAtt <= targetDimensionality) {
+            DataSet trivialResult;
+            try {
+                trivialResult = dset.copy();
+                return trivialResult;
+            } catch (Exception e) {
+                return dset;
+            }
+        }
+        // Generate a random projection matrix.
+        float[][] projectionMatrix = generateRandomProjectionMatrix(numFAtt,
+                targetDimensionality);
+        DataSet projectedDSet = new DataSet();
+        
