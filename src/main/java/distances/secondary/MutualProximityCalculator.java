fd7ac85be6fbe7a992498a4c02b7a9ca32f7f144
kvef <162893530+kvef@users.noreply.github.com>
2021-12-19 20:20:00 +0000

    Upgraded version

--- src/main/java/distances/secondary/MutualProximityCalculator.java
+++ src/main/java/distances/secondary/MutualProximityCalculator.java
@@ -63,4 +63,84 @@ implements Serializable {
      * @param cmet CombinedMetric object for distance calculations.
      */
     public MutualProximityCalculator(float[][] dMatPrimary, DataSet dset,
-            C
+            CombinedMetric cmet) {
+        this.dMatPrimary = dMatPrimary;
+        this.dset = dset;
+        this.cmet = cmet;
+        int other;
+        if (dMatPrimary != null && dMatPrimary.length > 0) {
+            distMeans = new double[dMatPrimary.length];
+            distStDevs = new double[dMatPrimary.length];
+            float[] numIncluded = new float[dMatPrimary.length];
+            // Calculate the means of distances from each point to other points.
+            for (int i = 0; i < dMatPrimary.length; i++) {
+                for (int j = 0; j < dMatPrimary[i].length; j++) {
+                    if (numIncluded[i] == 0) {
+                        numIncluded[i] = 1;
+                        distMeans[i] = dMatPrimary[i][j];
+                    } else {
+                        numIncluded[i]++;
+                        distMeans[i] = distMeans[i]
+                                * ((numIncluded[i] - 1) / numIncluded[i])
+                                + dMatPrimary[i][j] * (1 / numIncluded[i]);
+                    }
+                    other = i + j + 1;
+                    if (numIncluded[other] == 0) {
+                        numIncluded[other] = 1;
+                        distMeans[other] = dMatPrimary[i][j];
+                    } else {
+                        numIncluded[other]++;
+                        distMeans[other] = distMeans[other]
+                                * ((numIncluded[other] - 1)
+                                / numIncluded[other])
+                                + dMatPrimary[i][j] * (1 / numIncluded[other]);
+                    }
+                }
+            }
+            Arrays.fill(numIncluded, 0);
+            // Calculate the standard deviation of distances from each point to
+            // other points.
+            for (int i = 0; i < dMatPrimary.length; i++) {
+                for (int j = 0; j < dMatPrimary[i].length; j++) {
+                    if (numIncluded[i] == 0) {
+                        numIncluded[i] = 1;
+                        distStDevs[i] = (dMatPrimary[i][j] - distMeans[i])
+                                * (dMatPrimary[i][j] - distMeans[i]);
+                    } else {
+                        numIncluded[i]++;
+                        distStDevs[i] = distStDevs[i] * ((numIncluded[i] - 1)
+                                / numIncluded[i]) + (dMatPrimary[i][j]
+                                - distMeans[i]) * (dMatPrimary[i][j]
+                                - distMeans[i]) * (1 / numIncluded[i]);
+                    }
+                    other = i + j + 1;
+                    if (numIncluded[other] == 0) {
+                        numIncluded[other] = 1;
+                        distStDevs[other] = (dMatPrimary[i][j]
+                                - distMeans[other]) * (dMatPrimary[i][j]
+                                - distMeans[other]);
+                    } else {
+                        numIncluded[other]++;
+                        distStDevs[other] = distStDevs[other]
+                                * ((numIncluded[other] - 1)
+                                / numIncluded[other])
+                                + (dMatPrimary[i][j] - distMeans[other])
+                                * (dMatPrimary[i][j] - distMeans[other])
+                                * (1 / numIncluded[other]);
+                    }
+                }
+            }
+            for (int i = 0; i < distStDevs.length; i++) {
+                distStDevs[i] = Math.sqrt(distStDevs[i]);
+            }
+        }
+    }
+
+    /**
+     * Calculate the secondary distance matrix on the data in a multi-threaded
+     * way.
+     *
+     * @param nsf NeighborSetFinder object.
+     * @param numThreads Number of threads to use.
+     * @return float[][] representing the upper triangular secondary MP distance
+     * matri
