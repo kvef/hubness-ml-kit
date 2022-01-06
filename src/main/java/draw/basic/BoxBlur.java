5b8b4ee9381cd634d00f3d1377097c9d33868fa7
kvef <162893530+kvef@users.noreply.github.com>
2022-01-06 13:48:18 +0000

    remove useless dependencies

--- src/main/java/draw/basic/BoxBlur.java
+++ src/main/java/draw/basic/BoxBlur.java
@@ -23,4 +23,95 @@ import java.awt.Dimension;
  * neighboring pixels. The filter kernel used here has a cross shape instead of
  * square, the size of the kernel depends on radius.
  *
- *
+ * @author phu004 (http://www.java-gaming.org/index.php?;topic=24808.0),
+ * modified and extended by Nenad Tomasev <nenad.tomasev at gmail.com>
+ */
+public class BoxBlur {
+
+    private int radius;
+
+    /**
+     * @param radius Radius of the transformation range.
+     */
+    public BoxBlur(int radius) {
+        this.radius = radius;
+    }
+
+    /**
+     * Perform the box blur.
+     *
+     * @param img Image raster.
+     * @param offsSreenBuffer Buffer for off-screen processing.
+     * @param dim Dimension object.
+     */
+    public void blurPixels(int[] img, int[] offSreenBuffer, Dimension dim) {
+
+        // First blur the image in the horizontal direction then in the vertical
+        // direction.
+        horizontalBlur(img, offSreenBuffer, dim);
+        verticalBlur(offSreenBuffer, img, dim);
+
+        // Repeat once again for better quality.
+        horizontalBlur(img, offSreenBuffer, dim);
+        verticalBlur(offSreenBuffer, img, dim);
+
+    }
+
+    /**
+     * Perform the horizontal blur.
+     *
+     * @param originalPixels Original image raster.
+     * @param blurredPixels Blurred image raster.
+     * @param dim Dimension object.
+     */
+    public void horizontalBlur(int[] originalPixels,
+            int[] blurredPixels, Dimension dim) {
+        int width = dim.width;
+        int height = dim.height;
+        int sourcePosition, destPosition, rgb1, rgb2, tr, tg, tb, pixelCount;
+        for (int i = 0; i < height; i++) {
+            sourcePosition = i * width;
+            destPosition = i + height * (width - 1);
+            tr = 0;
+            tg = 0;
+            tb = 0;
+            for (int j = 0; j <= radius; j++) {
+                rgb1 = originalPixels[sourcePosition + j];
+                tr += ((rgb1 & 0xff0000) >> 16);
+                tg += ((rgb1 & 0x00ff00) >> 8);
+                tb += (rgb1 & 0xff);
+            }
+            pixelCount = radius + 1;
+            blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                    | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            sourcePosition++;
+            destPosition -= height;
+            pixelCount++;
+            for (int j = 1; j <= radius; j++, sourcePosition++,
+                    destPosition -= height, pixelCount++) {
+                rgb1 = originalPixels[sourcePosition + radius];
+                tr += ((rgb1 & 0xff0000) >> 16);
+                tg += ((rgb1 & 0x00ff00) >> 8);
+                tb += (rgb1 & 0xff);
+                blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                        | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            }
+            pixelCount--;
+            for (int j = radius + 1; j < width - radius; j++, sourcePosition++,
+                    destPosition -= height) {
+                rgb1 = originalPixels[sourcePosition + radius];
+                rgb2 = originalPixels[sourcePosition - radius - 1];
+                tr += (((rgb1 & 0xff0000) - (rgb2 & 0xff0000)) >> 16);
+                tg += (((rgb1 & 0x00ff00) - (rgb2 & 0x00ff00)) >> 8);
+                tb += ((rgb1 & 0xff) - (rgb2 & 0xff));
+                blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                        | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            }
+            pixelCount--;
+            for (int j = width - radius; j < width; j++, sourcePosition++,
+                    destPosition -= height, pixelCount--) {
+                rgb2 = originalPixels[sourcePosition - radius - 1];
+                tr -= ((rgb2 & 0xff0000) >> 16);
+                tg -= ((rgb2 & 0x00ff00) >> 8);
+                tb -= (rgb2 & 0xff);
+                blurredPixels[destPosition] = ((tr / pixelCount) << 16)
