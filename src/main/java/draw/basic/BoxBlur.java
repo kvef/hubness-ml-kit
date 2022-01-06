93e6839b2644e3cb9c3bb0107dd91040d6e42507
kvef <162893530+kvef@users.noreply.github.com>
2022-01-06 16:29:04 +0000

    variable

--- src/main/java/draw/basic/BoxBlur.java
+++ src/main/java/draw/basic/BoxBlur.java
@@ -115,3 +115,98 @@ public class BoxBlur {
                 tg -= ((rgb2 & 0x00ff00) >> 8);
                 tb -= (rgb2 & 0xff);
                 blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                        | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            }
+        }
+    }
+
+    /**
+     * Perform the vertical blur.
+     *
+     * @param originalPixels Original image raster.
+     * @param blurredPixels Blurred image raster.
+     * @param dim Dimension object.
+     */
+    public void verticalBlur(int[] originalPixels, int[] blurredPixels,
+            Dimension dim) {
+        int width = dim.width;
+        int height = dim.height;
+        int sourcePosition, destPosition, rgb1, rgb2, tr, tg, tb, pixelCount;
+        for (int i = 0; i < width; i++) {
+            sourcePosition = i * height;
+            destPosition = (width - 1) - i;
+            tr = 0;
+            tg = 0;
+            tb = 0;
+            for (int j = 0; j <= radius; j++) {
+                rgb1 = originalPixels[sourcePosition + j];
+                tr += ((rgb1 & 0xff0000) >> 16);
+                tg += ((rgb1 & 0x00ff00) >> 8);
+                tb += (rgb1 & 0xff);
+            }
+            pixelCount = radius + 1;
+            blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                    | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            sourcePosition++;
+            destPosition += width;
+            pixelCount++;
+            for (int j = 1; j <= radius; j++, sourcePosition++,
+                    destPosition += width, pixelCount++) {
+                rgb1 = originalPixels[sourcePosition + radius];
+                tr += ((rgb1 & 0xff0000) >> 16);
+                tg += ((rgb1 & 0x00ff00) >> 8);
+                tb += (rgb1 & 0xff);
+                blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                        | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            }
+            pixelCount--;
+            for (int j = radius + 1; j < height - radius; j++, sourcePosition++,
+                    destPosition += width) {
+                rgb1 = originalPixels[sourcePosition + radius];
+                rgb2 = originalPixels[sourcePosition - radius - 1];
+                tr += (((rgb1 & 0xff0000) - (rgb2 & 0xff0000)) >> 16);
+                tg += (((rgb1 & 0x00ff00) - (rgb2 & 0x00ff00)) >> 8);
+                tb += ((rgb1 & 0xff) - (rgb2 & 0xff));
+                blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                        | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            }
+            pixelCount--;
+            for (int j = height - radius; j < height; j++, sourcePosition++,
+                    destPosition += width, pixelCount--) {
+                rgb2 = originalPixels[sourcePosition - radius - 1];
+                tr -= ((rgb2 & 0xff0000) >> 16);
+                tg -= ((rgb2 & 0x00ff00) >> 8);
+                tb -= (rgb2 & 0xff);
+                blurredPixels[destPosition] = ((tr / pixelCount) << 16)
+                        | ((tg / pixelCount) << 8) | (tb / pixelCount);
+            }
+        }
+    }
+
+    /**
+     * Perform the box blur.
+     *
+     * @param img Image raster.
+     * @param offsSreenBuffer Buffer for off-screen processing.
+     * @param dim Dimension object.
+     */
+    public void blurPixelsWithAlpha(int[] img, int[] offSreenBuffer,
+            Dimension dim) {
+
+        //blur image in horizontal direction then vertical direction
+        horizontalBlurTransp(img, offSreenBuffer, dim);
+        verticalBlurTransp(offSreenBuffer, img, dim);
+
+        //repeat for another time for better quality
+        horizontalBlurTransp(img, offSreenBuffer, dim);
+        verticalBlurTransp(offSreenBuffer, img, dim);
+
+    }
+
+    /**
+     * Perform the horizontal blur while handling transparency and the alpha
+     * channel.
+     *
+     * @param originalPixels Original image raster.
+     * @param blurredPixels Blurred image raster.
+    
