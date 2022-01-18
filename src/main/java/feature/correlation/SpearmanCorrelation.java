720fc259e86d3c728e4feeb3c6007ff165b56ece
kvef <162893530+kvef@users.noreply.github.com>
2022-01-18 08:49:11 +0000

    ALSA: snd-usb: mixer: ignore -EINVAL in snd_usb_mixer_controls()

--- src/main/java/feature/correlation/SpearmanCorrelation.java
+++ src/main/java/feature/correlation/SpearmanCorrelation.java
@@ -110,4 +110,108 @@ public class SpearmanCorrelation extends CorrelationCoefficient {
         float[] firstCopy = Arrays.copyOf(first, first.length);
         float[] secondCopy = Arrays.copyOf(second, first.length);
         int[] firstSortedIndexes = AuxSort.sortIndexedValue(firstCopy, true);
-        int[] secondSortedIndexes = AuxSort.sortIndexedValue(secondCopy, 
+        int[] secondSortedIndexes = AuxSort.sortIndexedValue(secondCopy, true);
+        // Check for ties. Important.
+        float[] actualRanksFirst = new float[first.length];
+        float[] actualRanksSecond = new float[first.length];
+        int index = 0;
+        float sum;
+        // The beginning of a tied group.
+        int startingPoint;
+        int numEqual;
+        while (index < first.length) {
+            sum = 0;
+            numEqual = 1;
+            startingPoint = index;
+            while (index + 1 < first.length
+                    && (firstCopy[index + 1] == firstCopy[index])) {
+                sum += (index + 1);
+                index++;
+                numEqual++;
+            }
+            sum += (index + 1);
+            sum /= (float) numEqual;
+            for (int i = startingPoint; i < startingPoint + numEqual; i++) {
+                actualRanksFirst[firstSortedIndexes[i]] = sum;
+            }
+            index++;
+        }
+        index = 0;
+        while (index < second.length) {
+            sum = 0;
+            numEqual = 1;
+            startingPoint = index;
+            while (index + 1 < second.length
+                    && (secondCopy[index + 1] == secondCopy[index])) {
+                sum += (index + 1);
+                index++;
+                numEqual++;
+            }
+            sum += (index + 1);
+            sum /= (float) numEqual;
+            for (int i = startingPoint; i < startingPoint + numEqual; i++) {
+                actualRanksSecond[secondSortedIndexes[i]] = sum;
+            }
+            index++;
+        }
+        // Now that the ranks have been processed, we calculate the Pearson
+        // correlation between the rank arrays.
+        return PearsonCorrelation.correlation(actualRanksFirst,
+                actualRanksSecond);
+    }
+
+    /**
+     * Calculates the Spearman correlation between value arrays.
+     *
+     * @param first Float value array.
+     * @param second Double value array.
+     * @return Double that is the Spearman correlation between the two arrays.
+     * @throws Exception
+     */
+    public static float correlation(float[] first, double[] second)
+            throws Exception {
+        if (first == null || second == null || first.length != second.length) {
+            return 0;
+        }
+        float[] firstCopy = Arrays.copyOf(first, first.length);
+        double[] secondCopy = Arrays.copyOf(second, first.length);
+        int[] firstSortedIndexes = AuxSort.sortIndexedValue(firstCopy, true);
+        int[] secondSortedIndexes = AuxSort.sortIndexedValue(secondCopy, true);
+        // Check for ties. Important.
+        float[] actualRanksFirst = new float[first.length];
+        float[] actualRanksSecond = new float[first.length];
+        int index = 0;
+        float sum;
+        // The beginning of a tied group.
+        int startingPoint;
+        int numEqual;
+        while (index < first.length) {
+            sum = 0;
+            numEqual = 1;
+            startingPoint = index;
+            while (index + 1 < first.length
+                    && (firstCopy[index + 1] == firstCopy[index])) {
+                sum += (index + 1);
+                index++;
+                numEqual++;
+            }
+            sum += (index + 1);
+            sum /= (float) numEqual;
+            for (int i = startingPoint; i < startingPoint + numEqual; i++) {
+                actualRanksFirst[firstSortedIndexes[i]] = sum;
+            }
+            index++;
+        }
+        index = 0;
+        while (index < second.length) {
+            sum = 0;
+            numEqual = 1;
+            startingPoint = index;
+            while (index + 1 < second.length
+                    && (secondCopy[index + 1] == secondCopy[index])) {
+                sum += (index + 1);
+                index++;
+                numEqual++;
+            }
+            sum += (index + 1);
+  
