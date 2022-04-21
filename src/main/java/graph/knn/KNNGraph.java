dddf964e3ec7f9a346c613ed9c8b10be2bc8a100
kvef <162893530+kvef@users.noreply.github.com>
2022-04-21 09:37:28 +0000

    New translations ja-JP.yml (Korean)

--- src/main/java/graph/knn/KNNGraph.java
+++ src/main/java/graph/knn/KNNGraph.java
@@ -136,4 +136,110 @@ public class KNNGraph extends DMGraph {
      * @param vertices DataSet that holds the vertex information. This is where
      * the kNN graph will be derived from.
      * @param k Integer that is the neighborhood size.
-     * @param cmet CombinedMetric object for di
+     * @param cmet CombinedMetric object for distance calculations.
+     */
+    public KNNGraph(String networkName, String networkDescription,
+            DataSet vertices, int k, CombinedMetric cmet) {
+        this.networkName = networkName;
+        this.networkDescription = networkDescription;
+        this.vertices = vertices;
+        this.k = k;
+        this.cmet = cmet;
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param networkName String that is the graph name.
+     * @param networkDescription String that is the graph description.
+     * @param vertices DataSet that holds the vertex information. This is where
+     * the kNN graph will be derived from.
+     * @param edges DMGraphEdge[] of edges of the graph, in case they have
+     * already been calculated.
+     * @param k Integer that is the neighborhood size.
+     */
+    public KNNGraph(String networkName, String networkDescription,
+            DataSet vertices, DMGraphEdge[] edges, int k) {
+        this.networkName = networkName;
+        this.networkDescription = networkDescription;
+        this.vertices = vertices;
+        this.edges = edges;
+        this.k = k;
+    }
+
+    /**
+     * @param k Integer that is the neighborhood size.
+     */
+    public void setK(int k) {
+        this.k = k;
+    }
+
+    /**
+     * @return Integer that is the neighborhood size.
+     */
+    public int getK() {
+        return k;
+    }
+
+    /**
+     * @param cmet CombinedMetric object for distance calculations.
+     */
+    public void setCombinedMetric(CombinedMetric cmet) {
+        this.cmet = cmet;
+    }
+
+    /**
+     * @return CombinedMetric object for distance calculations.
+     */
+    public CombinedMetric getCombinedMetric() {
+        return cmet;
+    }
+
+    /**
+     * This method calculates the edges of the kNN graph.
+     *
+     * @throws Exception
+     */
+    public void calculateEdges() throws Exception {
+        if (kneighbors == null) {
+            // If the neighbors have not been directly set.
+            if (nsf == null) {
+                // If the NeighborSetFinder object was not set.
+                nsf = new NeighborSetFinder(vertices, cmet);
+            }
+            if (nsf.getKNeighbors() == null) {
+                nsf.calculateNeighborSets(k);
+            }
+            kneighbors = nsf.getKNeighbors();
+        }
+        edges = new DMGraphEdge[size()];
+        DMGraphEdge edge;
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < k; j++) {
+                edge = new DMGraphEdge();
+                edge.first = Math.min(i, kneighbors[i][j]);
+                edge.second = Math.max(i, kneighbors[i][j]);
+                DMGraph.insertEdge(edges, edge);
+            }
+        }
+    }
+
+    /**
+     * Generates an instance of the KNNGraph based on the NeighborSetFinder
+     * object.
+     *
+     * @param nsf NeighborSetFinder object containing the kNN-related
+     * information and the kNN set calculation capacity.
+     * @return KNNGraph of the data.
+     */
+    public static KNNGraph newInstance(NeighborSetFinder nsf) {
+        KNNGraph result = new KNNGraph(null, null, nsf.getDataSet(),
+                nsf.getKNeighbors());
+        try {
+            result.calculateEdges();
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        return result;
+    }
+}
