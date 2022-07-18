dbd2a866c72d2825ce7dc4371ee8ec9531f7f699
kvef <162893530+kvef@users.noreply.github.com>
2022-07-18 07:02:56 +0000

    Version 2.2.3

--- src/main/java/gui/images/QuantizedImageViewer.java
+++ src/main/java/gui/images/QuantizedImageViewer.java
@@ -539,4 +539,41 @@ public class QuantizedImageViewer extends javax.swing.JFrame {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
-     
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(QuantizedImageViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(QuantizedImageViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(QuantizedImageViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(QuantizedImageViewer.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new QuantizedImageViewer().setVisible(true);
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private gui.images.ImagePanel allQuantizedPanel;
+    private javax.swing.JPanel codebookProfilesPanel;
+    private javax.swing.JScrollPane codebookScrollPane;
+    private javax.swing.JLabel cvectLabel;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JLabel occCountLabel;
+    private javax.swing.JLabel occCountValueLabel;
+    private gui.images.ImagePanel originalImagePanel;
+    private javax.swing.JMenuItem saveOverallItem;
+    private javax.swing.JMenuItem saveSelectedItem;
+    private gui.images.ImagePanel selectedCodebookPanel;
+    // End of variables declaration//GEN-END:variables
+}
