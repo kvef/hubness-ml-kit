b499dedbb663d21cd606fdb7249bb4dd53a9abb7
kvef <162893530+kvef@users.noreply.github.com>
2022-07-20 16:14:24 +0000

    i2400m: add Intel 6150 device IDs

--- src/main/java/gui/synthetic/Visual2DdataGenerator.java
+++ src/main/java/gui/synthetic/Visual2DdataGenerator.java
@@ -208,4 +208,105 @@ public class Visual2DdataGenerator extends javax.swing.JFrame {
         hubnessLandscapeItem.setMnemonic(KeyEvent.VK_H);
         HubnessEntropyLandscapeItem = new javax.swing.JMenuItem();
         HubnessEntropyLandscapeItem.setMnemonic(KeyEvent.VK_E);
-        badHubnessInterpolatedItem = new javax.swing
+        badHubnessInterpolatedItem = new javax.swing.JMenuItem();
+        badHubnessInterpolatedItem.setMnemonic(KeyEvent.VK_B);
+        badHubnessKNNItem = new javax.swing.JMenuItem();
+        classMapsMenu = new javax.swing.JMenu();
+        knnDensityMenuItem = new javax.swing.JMenuItem();
+        knnDensityMenuItem.setMnemonic(KeyEvent.VK_K);
+        nhbnnProbMenuItem = new javax.swing.JMenuItem();
+        nhbnnProbMenuItem.setMnemonic(KeyEvent.VK_N);
+        hiknnInformationMapItem = new javax.swing.JMenuItem();
+        hiknnInformationMapItem.setMnemonic(KeyEvent.VK_H);
+        hiknnNonWeightedInfoItem = new javax.swing.JMenuItem();
+        hwKNNDensityMenuItem = new javax.swing.JMenuItem();
+        hFNNDensityMenuItem = new javax.swing.JMenuItem();
+        helpMenu = new javax.swing.JMenu();
+        helpMenu.setMnemonic(KeyEvent.VK_H);
+        aboutItem = new javax.swing.JMenuItem();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        classSelectButtons.add(class0Radio);
+        class0Radio.setText("Class 0");
+
+        classSelectButtons.add(class1Radio);
+        class1Radio.setText("Class 1");
+
+        classSelectButtons.add(class2Radio);
+        class2Radio.setText("Class 2");
+
+        classSelectButtons.add(class3Radio);
+        class3Radio.setText("Class 3");
+
+        classSelectButtons.add(class4Radio);
+        class4Radio.setText("Class 4");
+
+        classSelectButtons.add(class5Radio);
+        class5Radio.setText("Class 5");
+
+        classSelectButtons.add(class6Radio);
+        class6Radio.setText("Class 6");
+
+        classSelectButtons.add(class7Radio);
+        class7Radio.setText("Class 7");
+
+        classSelectButtons.add(class8Radio);
+        class8Radio.setText("Class 8");
+
+        classSelectButtons.add(class9Radio);
+        class9Radio.setText("Class 9");
+
+        classSelectButtons.add(class10Radio);
+        class10Radio.setText("Class 10");
+
+        classSelectButtons.add(class11Radio);
+        class11Radio.setText("Class 11");
+
+        classSelectButtons.add(class12Radio);
+        class12Radio.setText("Class 12");
+
+        classSelectButtons.add(class13Radio);
+        class13Radio.setText("Class 13");
+
+        classSelectButtons.add(class14Radio);
+        class14Radio.setText("Class 14");
+
+        addClassButton.setText("Add Class");
+        addClassButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                addClassButtonMouseClicked(evt);
+            }
+        });
+
+        xNameLabel.setText("X:");
+
+        yNameLabel.setText("Y:");
+
+        xLabel.setText("0");
+
+        yLabel.setText("0");
+
+        scalingNameLabel.setText("setScaling factor:");
+
+        drawDSPanel.setBackground(new java.awt.Color(255, 255, 255));
+        drawDSPanel.setBorder(new javax.swing.border.MatteBorder(null));
+        drawDSPanel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                drawDSPanelMouseClicked(evt);
+            }
+        });
+        drawDSPanel.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseMoved(java.awt.event.MouseEvent evt) {
+                drawDSPanelMouseMoved(evt);
+            }
+        });
+
+        javax.swing.GroupLayout drawDSPanelLayout = new javax.swing.GroupLayout(drawDSPanel);
+        drawDSPanel.setLayout(drawDSPanelLayout);
+        drawDSPanelLayout.setHorizontalGroup(
+            drawDSPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 868, Short.MAX_VALUE)
+        );
+        drawDSPanelLayout.setVerticalGroup(
+            drawDSPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADIN
