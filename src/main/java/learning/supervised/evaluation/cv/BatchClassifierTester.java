8d6a2c1b83d27575c73a85029f3225d9bab60802
kvef <162893530+kvef@users.noreply.github.com>
2022-10-29 13:26:56 +0000

    merge 5.5 => trunk

--- src/main/java/learning/supervised/evaluation/cv/BatchClassifierTester.java
+++ src/main/java/learning/supervised/evaluation/cv/BatchClassifierTester.java
@@ -107,4 +107,72 @@ public class BatchClassifierTester {
     private float noiseMin = 0, noiseMax = 0, noiseStep = 1, mlMin = 0,
             mlMax = 0, mlStep = 1;
     // Various files and directories used in the experiment.
-    private File inConfigFile, inDir, outDir, currOutDSD
+    private File inConfigFile, inDir, outDir, currOutDSDir, summaryDir,
+            inLabelFile, distancesDir, mlWeightsDir, foldsDir;
+    // A list of classifier names to use in the experiment.
+    private ArrayList<String> classifierNames = new ArrayList<>(10);
+    // Possible parameter value maps to use with certain algorithms.
+    HashMap<String, HashMap<String, Object>> algorithmParametrizationMap;
+    // Classifier prototype arrays.
+    ValidateableInterface[] nonDiscreteArray, discreteArray;
+    // List of paths to the datasets that the experiment is to be executed on.
+    private ArrayList<String> dsPaths = new ArrayList<>(100);
+    // List of CombinedMetric objects for distance calculations that correspond
+    // to different datasets. Different datasets might require different
+    // metrics to be used, so this is why it is necessary to explicitly specify
+    // a metric for each dataset.
+    private ArrayList<CombinedMetric> dsMetric = new ArrayList<>(100);
+    private ArrayList<Integer>[][][] allDataSetFolds;
+    // An alternative specification to the fold specification above, used for
+    // OpenML compatibility.
+    public ArrayList<Integer>[][][][] trainTestIndexes;
+    // Folds to use for testing on the dataset.
+    private ArrayList<Integer>[][] dsFolds;
+    // Class names are kept track of only for OpenML data sources, as the labels
+    // need to be specified in their original form (not just the indexes) when
+    // the predictions are uploaded. Class name arrays for non-OpenML data
+    // sources will be null in this structure.
+    String[][] classNames;
+    // DataSet objects.
+    private DataSet originalDSet, currDSet;
+    // The discretized data object.
+    private DiscretizedDataSet currDiscDSet;
+    // Current CombinedMetric object.
+    private CombinedMetric cmet;
+    // The current distance matrix, given as an upper triangular distance matrix
+    // so that each row i contains only the distances to j > i, encoded so that
+    // distMat[i][j] is the distance between i and i + j + 1. This is the
+    // standard matrix format throughout this library.
+    private float[][] distMat;
+    // A boolean flag array indicating which algorithms require discretized
+    // data objects.
+    private boolean[] isDiscreteAlgorithm;
+    // Whether there is a need for discretizing the data.
+    private boolean discreteExists = false;
+    // A label array prior to any mislabeling. It is used for evaluation, as
+    // classifiers that were trained on mislabeled data need to be evaluated on
+    // the exact labels.
+    private int[] originalLabels;
+    // Integer that is the number of categories in the data.
+    private int numCategories;
+    // Whether we are in the multi-label experimental mode, where we are testing
+    // different representations of the same underlying dataset that has
+    // multiple label distributions / classification problems defined on top.
+    private boolean multiLabelMode = false;
+    // The number of classification problems on the dataset.
+    private int numDifferentLabelings = 1;
+    // Separator in the label file.
+    private String lsep;
+    // Whether there are distance users in the continous or discretized case,
+    // indicating whether there is a need for calculating the distance matrix
+    // or not.
+    private boolean distUserPresentDisc = false;
+    private boolean distUserPresentNonDisc = false;
+    // Whether there is a need to calculate the kNN sets.
+    private boolean kNNUserPresent = false;
+    // Whether to use the approximate kNN sets and which quality parameter to
+    // use.
+    private float alphaAppKNNs = 1f;
+    private boolean approximateKNNs = false;
+    // Current instance selector.
+    private InstanceSel
