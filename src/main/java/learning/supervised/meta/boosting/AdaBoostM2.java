4dee549a5bf2aa3f1dab06a33c4cde8c1677dea0
kvef <162893530+kvef@users.noreply.github.com>
2022-11-13 17:12:55 +0000

    bang

--- src/main/java/learning/supervised/meta/boosting/AdaBoostM2.java
+++ src/main/java/learning/supervised/meta/boosting/AdaBoostM2.java
@@ -76,4 +76,106 @@ public class AdaBoostM2 extends Classifier implements
     private ArrayList<Double> pseudoLoss;
     // Loss ratios over the iterations, used to determine learner weights.
     private ArrayList<Double> lossRatios;
-    // Learn
+    // Learner predictions over the iterations.
+    private ArrayList<double[][]> predictions;
+    // For corrections when the weights approach the minimal double value too
+    // much.
+    private static final double SAFE_FACTOR = Math.pow(2, 50);
+    
+    @Override
+    public HashMap<String, String> getParameterNamesAndDescriptions() {
+        HashMap<String, String> paramMap = new HashMap<>();
+        paramMap.put("numIterationsTrain", "Number of iterations to use for"
+                + "boosting.");
+        HashMap<String, String> baseParamMap =
+                weakLearner.getParameterNamesAndDescriptions();
+        HashMap<String, String> resultingMap = new HashMap<>();
+        resultingMap.putAll(paramMap);
+        baseParamMap.putAll(baseParamMap);
+        return resultingMap;
+    }
+    
+    @Override
+    public Publication getPublicationInfo() {
+        JournalPublication pub = new JournalPublication();
+        pub.setTitle("A Decision-Theoretic Generalization of On-Line Learning "
+                + "and an Application to Boosting");
+        pub.addAuthor(new Author("Yoav", "Freund"));
+        pub.addAuthor(new Author("Robert E.", "Schapire"));
+        pub.setPublisher(Publisher.ACADEMIC_PRESS);
+        pub.setJournalName("Journal of Computer and System Sciences");
+        pub.setYear(1997);
+        pub.setStartPage(119);
+        pub.setEndPage(139);
+        pub.setVolume(55);
+        return pub;
+    }
+    
+    @Override
+    public long getVersion() {
+        return serialVersionUID;
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param weakLearner BoostableClassifier that is to be boosted.
+     */
+    public AdaBoostM2(BoostableClassifier weakLearner) {
+        this.weakLearner = weakLearner;
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param weakLearner BoostableClassifier that is to be boosted.
+     * @param numIterations Integer that is the number of boosting iterations.
+     */
+    public AdaBoostM2(BoostableClassifier weakLearner, int numIterations) {
+        this.weakLearner = weakLearner;
+        this.numIterationsTrain = numIterations;
+    }
+
+    /**
+     * Initialization.
+     *
+     * @param weakLearner BoostableClassifier that is to be boosted.
+     * @param numIterations Integer that is the number of boosting iterations.
+     * @param trainingData DataSet object to train the model on.
+     */
+    public AdaBoostM2(BoostableClassifier weakLearner, int numIterations,
+            DataSet trainingData) {
+        this.weakLearner = weakLearner;
+        this.numIterationsTrain = numIterations;
+        this.trainingData = trainingData;
+    }
+
+    @Override
+    public String getName() {
+        if (weakLearner != null) {
+            return "AdaBoostM2(" + weakLearner.getName() + ")";
+        } else {
+            return "AdaBoostM2";
+        }
+    }
+
+    @Override
+    public void train() throws Exception {
+        NeighborSetFinder nsf = getNSF();
+        float[][] dMat = getDistMatrix();
+        if (weakLearner instanceof NSFUserInterface && nsf == null) {
+            // Then we should calculate the NSF object, for latter speed-ups.
+            nsf = new NeighborSetFinder(trainingData, getCombinedMetric());
+            if (dMat != null) {
+                nsf.setDistances(dMat);
+            } else {
+                nsf.calculateDistances();
+            }
+            nsf.calculateNeighborSets(((NSFUserInterface)weakLearner).
+                    getNeighborhoodSize());
+        }
+        int numInstances = trainingData.size();
+        classPriors = trainingData.getClassPriors();
+        // Because of the interfaces it pays off to duplicate the matrix in a
+        // different format.
+    
