2e58e255fceeb9f2e858f9e02c183529809d6653
kvef <162893530+kvef@users.noreply.github.com>
2022-11-23 08:13:03 +0000

    fix rules

--- src/main/java/learning/supervised/meta/boosting/BoostableClassifier.java
+++ src/main/java/learning/supervised/meta/boosting/BoostableClassifier.java
@@ -11,4 +11,44 @@
 * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
 * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 *
-* You should have rec
+* You should have received a copy of the GNU General Public License along with
+* this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package learning.supervised.meta.boosting;
+
+import algref.Citable;
+import java.io.Serializable;
+import learning.supervised.Classifier;
+
+/**
+ * This class extends the usual Classifier class by introducing the instance
+ * weighting options to be used in re-weightable boosting procedures.
+ *
+ * @author Nenad Tomasev <nenad.tomasev at gmail.com>
+ */
+public abstract class BoostableClassifier extends Classifier
+implements Serializable, Citable {
+
+    private static final long serialVersionUID = 1L;
+    /**
+     * This method sets the instance weights prior to model training. The
+     * weights correspond to the instance difficulty and relevance in the model.
+     *
+     * @param instanceWeights double[] of instance weights to be set.
+     */
+    public abstract void setTotalInstanceWeights(double[] instanceWeights);
+
+    /**
+     * This method sets the current label misclassification cost distribution
+     * for each instance. If x has the label y, then instanceLabelWeights[x][y]
+     * should be zero, as no additional cost is associated with the proper
+     * classification (that information is provided in the other instance
+     * weighting method, for clarity). In any case, such values will be ignored.
+     * As for the remaining weights, they should sum up to 1 for each instance.
+     *
+     * @param instanceLabelWeights double[][] of the cost distributions over
+     * labels for each DataInstance in the training data.
+     */
+    public abstract void setMisclassificationCostDistribution(
+            double[][] instanceLabelWeights);
+}
