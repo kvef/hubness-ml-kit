d545b5582fa1bbd9f07f53d439a43027285a3344
kvef <162893530+kvef@users.noreply.github.com>
2023-07-06 06:25:51 +0000

    Added missing license header

--- src/main/java/learning/unsupervised/Cluster.java
+++ src/main/java/learning/unsupervised/Cluster.java
@@ -61,4 +61,133 @@ public class Cluster implements Serializable {
      */
     public Cluster(DataSet dset) {
         this.dataContext = dset;
-        in
+        indexes = new ArrayList<>(Math.max(dset.size(), 10));
+    }
+
+    /**
+     * @constructor @param dset Dataset the indexes will point to.
+     * @param initSize Initial size of the index array.
+     */
+    public Cluster(DataSet dset, int initSize) {
+        this.dataContext = dset;
+        indexes = new ArrayList<>(Math.max(initSize, 10));
+    }
+
+    /**
+     * @return Indexes of instances within the cluster.
+     */
+    public ArrayList<Integer> getIndexes() {
+        return indexes;
+    }
+
+    /**
+     * @param indexes Indexes of instances within the cluster.
+     */
+    public void setIndexes(ArrayList<Integer> indexes) {
+        this.indexes = indexes;
+    }
+
+    /**
+     * @return DataSet that the instances belong to.
+     */
+    public DataSet getDefinitionDataset() {
+        return dataContext;
+    }
+
+    /**
+     * @param DataSet that the instances belong to.
+     */
+    public void setDefinitionDataset(DataSet dset) {
+        this.dataContext = dset;
+    }
+
+    /**
+     * @param dset DataSet describing the data from within the cluster.
+     * @param data ArrayList of data that is assigned to the current cluster.
+     */
+    public Cluster(DataSet dset, ArrayList<Integer> indexes) {
+        this.dataContext = dset;
+        this.indexes = indexes;
+    }
+
+    /**
+     *
+     * @param index Index within the cluster.
+     * @return Index within the embedding dataset.
+     */
+    public int getWithinDataSetIndexOf(int index) {
+        if (index < 0 || index > indexes.size()) {
+            return -1;
+        }
+        return indexes.get(index);
+    }
+
+    /**
+     * @param index Index within the cluster.
+     * @param dsIndex Index within dataset.
+     */
+    public void setWithinDataSetIndexOf(int index, int dsIndex) {
+        indexes.set(index, dsIndex);
+    }
+
+    /**
+     * @return All data instances contained within the cluster.
+     */
+    public ArrayList<DataInstance> getAllInstances() {
+        ArrayList<DataInstance> data =
+                new ArrayList<>(indexes.size());
+        for (int index : indexes) {
+            data.add(dataContext.getInstance(index));
+        }
+        return data;
+    }
+
+    /**
+     * @param index Index within the cluster.
+     * @return Corresponding DataInstance.
+     */
+    public DataInstance getInstance(int index) {
+        return dataContext.getInstance(indexes.get(index));
+    }
+
+    /**
+     *
+     * @return Cluster that contains the entire dataset.
+     */
+    public static Cluster fromEntireDataset(DataSet dset) {
+        ArrayList<Integer> indexes = new ArrayList<>(dset.size());
+        for (int i = 0; i < dset.size(); i++) {
+            indexes.add(i);
+        }
+        return new Cluster(dset, indexes);
+    }
+
+    /**
+     * @param index Index of the instance to be added to the cluster.
+     */
+    public void addInstance(int index) {
+        indexes.add(index);
+    }
+
+    /**
+     *
+     * @param configuration Cluster configuration.
+     * @param dataset Data set.
+     * @return Cluster associations for a given clustering configuration.
+     */
+    public static int[] getAssociationsForClustering(Cluster[] configuration,
+            DataSet dataset) {
+        int[] clusterAssociations = new int[dataset.size()];
+        for (int cIndex = 0; cIndex < configuration.length; cIndex++) {
+            Cluster c = configuration[cIndex];
+            for (int index : c.indexes) {
+                clusterAssociations[index] = cIndex;
+            }
+        }
+        return clusterAssociations;
+    }
+
+    /**
+     * @param associations Integer array representing cluster associations.
+     * @param dset Data set.
+     * 
