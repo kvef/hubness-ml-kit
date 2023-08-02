06ff5e24fc1cc83c1bc3be943a4d8e8c013fd484
kvef <162893530+kvef@users.noreply.github.com>
2023-08-02 09:17:16 +0000

    added some icons

--- src/main/java/learning/unsupervised/ClustererFactory.java
+++ src/main/java/learning/unsupervised/ClustererFactory.java
@@ -49,4 +49,90 @@ public class ClustererFactory {
     
     /**
      * This is how initial clusterer instances are currently generated, as they
-   
+     * require initial parametrizations and doing all of that via reflection in
+     * every single case might be somewhat non-trivial in the general case -
+     * though it is certainly preferable to having the algorithm names hardcoded
+     * inside. TODO: change this clusterer initialization mechanism.
+     *
+     * @param cName Clusterer name.
+     * @param dset DataSet object.
+     * @param k Neighborhood size.
+     * @param distMat Distance matrix.
+     * @param nsf NeighborSetFinder object for kNN sets.
+     * @param trainingKernelMat Kernel matrix.
+     * @param nsfKernel Kernel kNN object.
+     * @param numClusters Number of clusters.
+     * @param cmet CombinedMetric object for distance calculations.
+     * @param ker Kernel object for kernel mappings.
+     * @return
+     */
+    public static ClusteringAlg getClustererForName(
+            String cName,
+            DataSet dset,
+            int k,
+            float[][] distMat,
+            NeighborSetFinder nsf,
+            float[][] trainingKernelMat,
+            NeighborSetFinder nsfKernel,
+            int nClust,
+            CombinedMetric cmet,
+            Kernel ker) {
+        ClusteringAlg clusterer = null;
+        String algName = cName.toLowerCase();
+        switch (algName) {
+            case "ghpc":
+            case "learning.unsupervised.methods.ghpc":
+                clusterer = new GHPC(dset, cmet, nClust, k);
+                ((GHPC) clusterer).setDistMatrix(distMat);
+                ((GHPC) clusterer).setNSF(nsf);
+                break;
+            case "sup-ghpc":
+                clusterer = new GHPC(dset, cmet, nClust, k);
+                ((GHPC) clusterer).setDistMatrix(distMat);
+                ((GHPC) clusterer).setHubnessMode(GHPC.SUPERVISED);
+                ((GHPC) clusterer).setNSF(nsf);
+                break;
+            case "gkh":
+            case "learning.unsupervised.methods.gkh":
+                clusterer = new GKH(dset, cmet, nClust, k);
+                ((GKH) clusterer).setDistMatrix(distMat);
+                ((GKH) clusterer).setNSF(nsf);
+                break;
+            case "ghpkm":
+            case "learning.unsupervised.methods.ghpkm":
+                clusterer = new GHPKM(dset, cmet, nClust, k);
+                ((GHPKM) clusterer).setDistMatrix(distMat);
+                ((GHPKM) clusterer).setNSF(nsf);
+                break;
+            case "sup-ghpkm":
+                clusterer = new GHPKM(dset, cmet, nClust, k);
+                ((GHPKM) clusterer).setDistMatrix(distMat);
+                ((GHPKM) clusterer).setHubnessMode(GHPC.SUPERVISED);
+                ((GHPKM) clusterer).setNSF(nsf);
+                break;
+            case "kernel-ghpkm":
+            case "ker-ghpkm":
+            case "kernelghpkm":
+            case "learning.unsupervised.methods.kernelghpkm":
+                clusterer = new KernelGHPKM(dset, cmet, ker, nClust, k);
+                ((KernelGHPKM) clusterer).setDistMatrix(distMat);
+                ((KernelGHPKM) clusterer).setNSF(nsfKernel);
+                break;
+            case "lhpc":
+            case "learning.unsupervised.methods.lhpc":
+                clusterer = new LHPC(dset, cmet, nClust, k);
+                ((LHPC) clusterer).setDistMatrix(distMat);
+                break;
+            case "lkh":
+            case "learning.unsupervised.methods.lkh":
+                clusterer = new LKH(dset, cmet, nClust, k);
+                ((LKH) clusterer).setDistMatrix(distMat);
+                break;
+            case "kmeans":
+            case "learning.unsupervised.methods.kmeans":
+                clusterer = new KMeans(dset, cmet, nClust);
+                break;
+            case "kernel-kmeans":
+            case "ker-kmeans":
+            case "kernelkmeans":
+            case "learning.unsupervised.methods.kernelk
