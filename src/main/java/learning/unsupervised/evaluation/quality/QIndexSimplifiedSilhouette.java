c38f0f3951e37fbc1431cb8160fa1c91ccadb63c
kvef <162893530+kvef@users.noreply.github.com>
2023-08-19 21:24:36 +0000

    Update vim plugins

--- src/main/java/learning/unsupervised/evaluation/quality/QIndexSimplifiedSilhouette.java
+++ src/main/java/learning/unsupervised/evaluation/quality/QIndexSimplifiedSilhouette.java
@@ -73,4 +73,109 @@ public class QIndexSimplifiedSilhouette extends ClusteringQualityIndex {
 
     /**
      * @param numClusters Number of clusters in the configuration.
-     * @param clusterAssociations Clust
+     * @param clusterAssociations Cluster association array for the points.
+     * @param dset DataSet object.
+     */
+    public QIndexSimplifiedSilhouette(int numClusters,
+            int[] clusterAssociations, DataSet dset) {
+        this.clusterAssociations = clusterAssociations;
+        this.numClusters = numClusters;
+        setDataSet(dset);
+        cmet = CombinedMetric.EUCLIDEAN;
+    }
+
+    /**
+     * @param numClusters Number of clusters in the configuration.
+     * @param clusterAssociations Cluster association array for the points.
+     * @param dset DataSet object.
+     * @param cmet Metric to use for estimating the quality.
+     */
+    public QIndexSimplifiedSilhouette(int numClusters,
+            int[] clusterAssociations, DataSet dset, CombinedMetric cmet) {
+        this.clusterAssociations = clusterAssociations;
+        this.numClusters = numClusters;
+        setDataSet(dset);
+        this.cmet = cmet;
+    }
+
+    /**
+     *
+     * @param clusteringConfiguration Cluster configuration.
+     * @param dset DataSet object.
+     * @param cmet Metric to use for estimating the quality.
+     */
+    public QIndexSimplifiedSilhouette(Cluster[] clusteringConfiguration,
+            DataSet dset, CombinedMetric cmet) {
+        setClusters(clusteringConfiguration);
+        setDataSet(dset);
+        clusterAssociations = Cluster.getAssociationsForClustering(
+                clusteringConfiguration, dset);
+        numClusters = clusteringConfiguration == null
+                ? 0 : clusteringConfiguration.length;
+        this.cmet = cmet;
+    }
+
+    /**
+     * @return Silhouette values for all instances.
+     */
+    public float[] getInstanceSilhouetteArray() {
+        return instanceSilhouetteArray;
+    }
+
+    /**
+     * @return Silhouette values for all clusters.
+     */
+    public float[] getClusterSilhouetteArray() {
+        return clusterSilhouetteArray;
+    }
+
+    /**
+     * @param cIndex Cluster index.
+     * @return Silhouette value for the specified cluster.
+     */
+    public float getSilhouetteForCluster(int cIndex) {
+        return clusterSilhouetteArray[cIndex];
+    }
+    
+    @Override
+    public float validity() throws Exception {
+        float resultingIndex = 0f;
+        if (clusterAssociations == null) {
+            throw new Exception("Null cluster associations array. "
+                    + "No configuration to evaluate.");
+        }
+        int dataSize = clusterAssociations.length;
+        DataSet instances = getDataSet();
+        // Initialize and populate the non-noisy index lists.
+        ArrayList<Integer>[] clusterIndexes =
+                new ArrayList[numClusters];
+        for (int cIndex = 0; cIndex < numClusters; cIndex++) {
+            clusterIndexes[cIndex] = new ArrayList<>(
+                    Math.max(20, clusterAssociations.length / numClusters));
+        }
+        for (int i = 0; i < instances.size(); i++) {
+            if (clusterAssociations[i] >= 0 &&
+                    !instances.getInstance(i).isNoise()) {
+                clusterIndexes[clusterAssociations[i]].add(i);
+            }
+        }
+        // Get the cluster centroids.
+        DataInstance[] clusterCentroids = new DataInstance[numClusters];
+        Cluster[] clusters = new Cluster[numClusters];
+        for (int cIndex = 0; cIndex < numClusters; cIndex++) {
+            clusters[cIndex] = new Cluster(instances);
+            for (int index: clusterIndexes[cIndex]) {
+                clusters[cIndex].addInstance(index);
+            }
+            clusterCentroids[cIndex] = clusters[cIndex].getCentroid();
+        }
+        int trueDataSize = 0;
+        ATOTAL = 0;
+        BTOTAL = 0;
+        HATOTAL = 0;
+        HBTOTAL = 0;
+        AHATOTAL = 0;
+        AHBTOTAL = 0;
+        REGATOTAL = 0;
+        REGBTOTAL = 0;
+        float[] e
