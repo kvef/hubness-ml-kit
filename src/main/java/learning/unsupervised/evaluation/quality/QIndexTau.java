2ae9cffa28a6abdb57342c5f2af6ce78d4010395
kvef <162893530+kvef@users.noreply.github.com>
2023-08-20 18:04:45 +0000

    New design

--- src/main/java/learning/unsupervised/evaluation/quality/QIndexTau.java
+++ src/main/java/learning/unsupervised/evaluation/quality/QIndexTau.java
@@ -45,4 +45,103 @@ public class QIndexTau extends ClusteringQualityIndex {
      */
     public QIndexTau(int[] clusterAssociations,
             DataSet dset) {
-        this.clusterAssociations = clusterAssociation
+        this.clusterAssociations = clusterAssociations;
+        setDataSet(dset);
+        cmet = CombinedMetric.EUCLIDEAN;
+    }
+
+    /**
+     * Initialization.
+     * 
+     * @param clusterAssociations Cluster association array for the points.
+     * @param dset DataSet object.
+     * @param cmet CombinedMetric object for distance calculations.
+     */
+    public QIndexTau(int[] clusterAssociations,
+            DataSet dset, CombinedMetric cmet) {
+        this.clusterAssociations = clusterAssociations;
+        setDataSet(dset);
+        this.cmet = cmet;
+    }
+
+    /**
+     * @param distances float[][] representing the upper triangular distance 
+     * matrix of the data.
+     */
+    public void setDistanceMatrix(float[][] distances) {
+        this.distances = distances;
+        this.dGiven = true;
+    }
+    
+    @Override
+    public float validity() throws Exception {
+        DataSet instances = getDataSet();
+        if (!dGiven) {
+            distances = instances.calculateDistMatrix(cmet);
+        }
+        Cluster[] clusterConfiguration =
+                Cluster.getConfigurationFromAssociations(clusterAssociations,
+                instances);
+        int numIntraDists = 0;
+        int numInterDists = 0;
+        int numClusters = clusterConfiguration.length;
+        if (numClusters < 2) {
+            return 0;
+        }
+        for (int c1 = 0; c1 < numClusters; c1++) {
+            if (clusterConfiguration[c1].size() > 0) {
+                numIntraDists += clusterConfiguration[c1].size()
+                        * (clusterConfiguration[c1].size() - 1) / 2;
+                for (int c2 = 0; c2 < numClusters; c2++) {
+                    if (clusterConfiguration[c2].size() > 0) {
+                        numInterDists += clusterConfiguration[c1].size()
+                                * clusterConfiguration[c2].size();
+                    }
+                }
+            }
+        }
+        float[] intraDists = new float[numIntraDists];
+        float[] interDists = new float[numInterDists];
+        int intraIndex = -1;
+        int interIndex = -1;
+        int numNoisyPoints = 0;
+        for (int i = 0; i < distances.length; i++) {
+            if (instances.getInstance(i).isNoise() ||
+                    clusterAssociations[i] < 0) {
+                numNoisyPoints++;
+                continue;
+            }
+            for (int j = 0; j < distances[i].length; j++) {
+                if (instances.getInstance(j).isNoise()) {
+                    continue;
+                }
+                if (clusterAssociations[i] == clusterAssociations[i + j + 1]) {
+                    intraDists[++intraIndex] = distances[i][j];
+                } else {
+                    interDists[++interIndex] = distances[i][j];
+                }
+            }
+        }
+        Arrays.sort(intraDists); //ascending sort
+        Arrays.sort(interDists); //ascending sort
+        intraIndex = 0;
+        interIndex = 0;
+        // This is the sum of concordant and discordant pairs.
+        long Nd = 0; // Num discordant pairs.
+        long Nc; // Num concordant pairs.
+        // We will only count the breaches.
+        long totalDists = numIntraDists + numInterDists;
+        long totalIntraInterPairs = numIntraDists * numInterDists;
+        do {
+            while (intraIndex < numIntraDists
+                    && intraDists[intraIndex] < interDists[interIndex]) {
+                intraIndex++;
+            }
+            if (intraIndex == numIntraDists) {
+                break;
+            }
+            // Then the interDists[interIndex] is discordant with all
+            // intraDists[i] for i >= intraIndex. There are numIntraDists -
+            // intraIndex of them.
+            Nd += (numIntraDists - intraIndex);
+            /
