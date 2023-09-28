6ed0e22844ab8721a1900774028838ffda0ae4e4
kvef <162893530+kvef@users.noreply.github.com>
2023-09-28 11:40:19 +0000

    fix copypasta

--- src/main/java/linear/matrix/SymmetricFloatMatrix.java
+++ src/main/java/linear/matrix/SymmetricFloatMatrix.java
@@ -51,4 +51,130 @@ public class SymmetricFloatMatrix implements DataMatrixInterface {
     public SymmetricFloatMatrix(int dim) {
         data = new float[dim][];
         for (int i = 0; i < dim; i++) {
-           
+            data[i] = new float[dim - i];
+        }
+    }
+
+    /**
+     * @param symmetricMatrix Two-dimensional float array that is the row
+     * representation of the symmetric float matrix.
+     */
+    public SymmetricFloatMatrix(float[][] symmetricMatrix) {
+        this.data = symmetricMatrix;
+    }
+
+    @Override
+    public boolean isSymmetricMatrixImplementation() {
+        return true;
+    }
+
+    @Override
+    public boolean isSquare() {
+        return true;
+    }
+
+    @Override
+    public float getElementAt(int row, int col) {
+        if (row <= col) {
+            return data[row][col - row];
+        } else {
+            return data[col][row - col];
+        }
+    }
+
+    @Override
+    public void setElementAt(int row, int col, float newValue) {
+        if (row <= col) {
+            data[row][col - row] = newValue;
+        } else {
+            data[col][row - col] = newValue;
+        }
+    }
+
+    @Override
+    public int numberOfRows() {
+        return data.length;
+    }
+
+    @Override
+    public int numberOfColumns() {
+        return data.length;
+    }
+
+    @Override
+    public float[] getRow(int row) {
+        // Creates an entire row as if the matrix wasn't only half-filled.
+        float[] result = new float[data.length];
+        for (int i = 0; i < row; i++) {
+            result[i] = data[i][row - i];
+        }
+        for (int i = row; i < data.length; i++) {
+            result[i] = data[row][i - row];
+        }
+        return result;
+    }
+
+    @Override
+    public float[] getColumn(int col) {
+        // Creates an entire column as if the matrix wasn't only half-filled.
+        float[] result = new float[data.length];
+        for (int i = 0; i < col; i++) {
+            result[i] = data[i][col - i];
+        }
+        for (int i = col; i < data.length; i++) {
+            result[i] = data[col][i - col];
+        }
+        return result;
+    }
+
+    @Override
+    public void setRow(int row, float[] newValues) throws Exception {
+        for (int i = 0; i < row; i++) {
+            data[i][row - i] = newValues[i];
+        }
+        for (int i = row; i < data.length; i++) {
+            data[row][i - row] = newValues[i];
+        }
+    }
+
+    @Override
+    public void setColumn(int col, float[] newValues) throws Exception {
+        for (int i = 0; i < col; i++) {
+            data[i][col - i] = newValues[i];
+        }
+        for (int i = col; i < data.length; i++) {
+            data[col][i - col] = newValues[i];
+        }
+    }
+
+    @Override
+    public DataMatrixInterface calculateInverse() throws Exception {
+        DataMatrixInterface result = new DataMatrix(data.length, data.length);
+        if (data.length == 1) {
+            if (data[0][0] != 0) {
+                result.setElementAt(0, 0, 1 / data[0][0]);
+            } else {
+                return null;
+            }
+        }
+        int[] indexes;
+        for (int i = 0; i < data.length; i++) {
+            for (int j = 0; j < data.length; j++) {
+                indexes = new int[data.length - 1];
+                for (int k = 0; k < j; k++) {
+                    indexes[k] = k;
+                }
+                for (int k = j + 1; k < data.length; k++) {
+                    indexes[k - 1] = k;
+                }
+                if (i == 0) {
+                    result.setElementAt(i, j, calculateCofactor(indexes, i, 1));
+                } else {
+                    result.setElementAt(i, j, calculateCofactor(indexes, i, 0));
+                }
+            }
+        }
+        // Now calculate determinant from first row minors.
+        float detValue = 0;
+        for (int i = 0; i < data.length; i++) {
+          
