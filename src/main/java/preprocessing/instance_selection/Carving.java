66cbca5953cb40ba67cc3f030157d8c261077465
kvef <162893530+kvef@users.noreply.github.com>
2024-01-27 19:01:55 +0000

    add some stuff

--- src/main/java/preprocessing/instance_selection/Carving.java
+++ src/main/java/preprocessing/instance_selection/Carving.java
@@ -581,4 +581,55 @@ public class Carving extends InstanceSelector implements NSFUserInterface {
                     if (currLabel == originalDataSet.getLabelOf(
                             protoIndexes.get(kneighbors[i][j]))) {
                         protoGoodHubness[kneighbors[i][j]]++;
-                    } else
+                    } else {
+                        protoBadHubness[kneighbors[i][j]]++;
+                    }
+                    protoHubness[kneighbors[i][j]]++;
+                    protoClassHubness[currLabel][kneighbors[i][j]]++;
+                }
+            }
+            setProtoNeighborSets(kneighbors);
+        } else {
+            // In this case, no prior neighbor information is available, so
+            // we just proceed in a simple way.
+            this.setNeighborhoodSize(k);
+            DataSet originalDataSet = getOriginalDataSet();
+            ArrayList<Integer> prototypeIndexes = getPrototypeIndexes();
+            int[][] kneighbors = new int[originalDataSet.size()][k];
+            // Make a subcollection.
+            DataSet tCol = originalDataSet.cloneDefinition();
+            tCol.data = new ArrayList<>(prototypeIndexes.size());
+            for (int index : prototypeIndexes) {
+                tCol.data.add(originalDataSet.getInstance(index));
+            }
+            int numClasses = getNumClasses();
+            int[] protoHubness = new int[prototypeIndexes.size()];
+            int[] protoGoodHubness = new int[prototypeIndexes.size()];
+            int[] protoBadHubness = new int[prototypeIndexes.size()];
+            int[][] protoClassHubness =
+                    new int[numClasses][prototypeIndexes.size()];
+            setPrototypeHubness(protoHubness);
+            setPrototypeGoodHubness(protoGoodHubness);
+            setPrototypeBadHubness(protoBadHubness);
+            setProtoClassHubness(protoClassHubness);
+            int currLabel;
+            int protoLabel;
+            CombinedMetric cmet = this.getCombinedMetric();
+            for (int i = 0; i < originalDataSet.size(); i++) {
+                currLabel = originalDataSet.getLabelOf(i);
+                kneighbors[i] = NeighborSetFinder.getIndexesOfNeighbors(tCol,
+                        originalDataSet.getInstance(i), k, cmet);
+                for (int nIndex : kneighbors[i]) {
+                    protoClassHubness[currLabel][nIndex]++;
+                    protoHubness[nIndex]++;
+                    protoLabel = originalDataSet.getLabelOf(nIndex);
+                    if (protoLabel == currLabel) {
+                        protoGoodHubness[nIndex]++;
+                    } else {
+                        protoBadHubness[nIndex]++;
+                    }
+                }
+            }
+        }
+    }
+}
