326ddfdf0f66437ba3473e4984606c683f3cc669
kvef <162893530+kvef@users.noreply.github.com>
2024-05-01 15:04:26 +0000

    publish muk_web_theme - 12.0

--- src/main/java/util/SOPLUtil.java
+++ src/main/java/util/SOPLUtil.java
@@ -8,4 +8,137 @@
 * version.
 * 
 * This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERC
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package util;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+/**
+ * A utility class that contains methods for printing our data arrays.
+ *
+ * @author Nenad Tomasev <nenad.tomasev at gmail.com>
+ */
+public class SOPLUtil {
+
+    /**
+     * Prints out the array to standard output.
+     *
+     * @param arr An array of float values.
+     */
+    public static void printArray(float[] arr) {
+        if (arr == null) {
+            System.out.println("Null array.");
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print(arr[arr.length - 1]);
+        System.out.println();
+    }
+
+    /**
+     * Prints out the array to standard output.
+     *
+     * @param arr An array of integer values.
+     */
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            System.out.println("Null array.");
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print(arr[arr.length - 1]);
+        System.out.println();
+    }
+
+    /**
+     * Prints out the array to standard output.
+     *
+     * @param arr An array of double values.
+     */
+    public static void printArray(double[] arr) {
+        if (arr == null) {
+            System.out.println("Null array.");
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print(arr[arr.length - 1]);
+        System.out.println();
+    }
+
+    /**
+     * Prints out the array to standard output.
+     *
+     * @param arr An array of string values.
+     */
+    public static void printArray(String[] arr) {
+        if (arr == null) {
+            System.out.println("Null array.");
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print(arr[arr.length - 1]);
+        System.out.println();
+    }
+
+    /**
+     * Prints out the ArrayList to standard output.
+     *
+     * @param arr An ArrayList of double values.
+     */
+    public static void printArrayList(ArrayList arr) {
+        if (arr == null) {
+            System.out.println("Null ArrayList.");
+            return;
+        }
+        if (arr.isEmpty()) {
+            System.out.println("Empty ArrayList");
+            return;
+        }
+        for (int i = 0; i < arr.size() - 1; i++) {
+            System.out.print(arr.get(i) + " ");
+        }
+        System.out.print(arr.get(arr.size() - 1));
+        System.out.println();
+    }
+
+    /**
+     * Prints out the array to the specified stream output.
+     *
+     * @param arr An array of double values.
+     * @param pw PrintWriter object for output.
+     */
+    public static void printArrayToStream(double[] arr, PrintWriter pw) {
+        if (arr == null) {
+            pw.println("Null array.");
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            pw.print(arr[i] + " ");
+        }
+        pw.print(arr[arr.length - 1]);
+        pw.println();
+    }
+
+    /**
+     * Prints out the array to the specified stream output.
+     *
+     * @param arr An array of integer values.
+     * @param pw PrintWriter object for output.
+     */
+    public static void printArrayToStream(int[] arr, PrintWriter pw) {
+        if (arr == null) {
+            pw.println("Null
